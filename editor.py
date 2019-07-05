29339f1e21a1a385b11d63be1278959cd0a2ad19
Medeiros, Joao Pedro <joao.pedro.medeiros@accenture.com>
2019-07-05 10:04:01 -0300

    Permitindo salvar texto inserido

--- editor.py
+++ editor.py
@@ -1,7 +1,19 @@
 from tkinter import *
+import tkinter.filedialog as fileDialog
 
 root = Tk("Editor de Texto")
-text =  Text(root)
+text = Text(root)
 text.grid()
 
+def saveas():
+    global text_editor
+    text_editor = text.get("1.0", "end-1c")
+    savelocation = fileDialog.asksaveasfilename()
+
+    file1 = open(savelocation, "w+")
+    file1.write(text_editor)
+    file1.close()
+
+button = Button(root, text="Save", command=saveas)
+button.grid()
 root.mainloop()
